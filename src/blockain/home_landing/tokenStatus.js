ea6a0075839c6600e55b86e6f557c1300ae0caa0
Marcos Pacheco <marcossilvano496@gmail.com>
2022-02-22 18:14:35 -0300

    chore: simple line chart

--- src/blockain/home_landing/tokenStatus.js
+++ src/blockain/home_landing/tokenStatus.js
@@ -2,11 +2,16 @@ import { Grid, Typography } from "@mui/material";
 import { Fragment } from "react";
 import LatestBlock from "../components/latestBlock";
 import Transferences from "../components/transfers";
+import LineChart from "../../charts/linecharts";
 
 const TokenStatus = () => {
+    const testLinechart = [[2, 150 * 100], [48, 110 * 100], [96, 89 * 100], [142, 100 * 100], [168, 50 * 100], [188, 100 * 100], [252, 120 * 100], [298, 140 * 100]]
     return <Fragment>
-        <Typography variant="subtitle2">token status</Typography>
+        <Typography variant='subtitle2'>token status</Typography>
         <Grid container spacing={2}>
+            <Grid item xs={12} md={6}>
+                <LineChart title='transactions' data={testLinechart} />
+            </Grid>
             <Grid item xs={12} md={6}><LatestBlock /></Grid>
             <Grid item xs={12} md={6}><Transferences /></Grid>
         </Grid>
