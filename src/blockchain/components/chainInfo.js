24895359549acd5b179c7c653660708ee8e5a0be
pixellab <pixellab@gmail.com>
2022-03-03 06:57:03 -0800

    chore: formatting for chain data

--- src/blockchain/components/chainInfo.js
+++ src/blockchain/components/chainInfo.js
@@ -1,6 +1,11 @@
-import { Container, Grid, Typography } from '@mui/material';
-import Card from '@mui/material/Card';
+import { 
+    Box, 
+    Grid, 
+    Typography 
+} from '@mui/material';
 import { useState } from 'react';
+
+import { Item, Data } from './chainInfo.styles';
 
 const data = {
     "items": [
@@ -11,33 +16,33 @@ const data = {
         { "title": "total issuance", "value": "1006B" },
         { "title": "nominators", "value": "53/53" },
         { "title": "validators", "value": "874609" },
-        { "title": "infration rate", "value": "7.86" },
+        { "title": "infration rate", "value": "7.86%" },
     ]
 }
 
 const ChainInfoCard = (title, value) => {
     return (
         <Grid item xs={6} sm={3} md={3} key={title}>
-            <Card className="card" >
-                <Typography>{title}</Typography>
-                <Typography variant='subtitle2'>{value}</Typography>
-            </Card>
+            <Item elevation={0} >
+                <Typography variant="h4">{title}</Typography>
+                <Data>{value}</Data>
+            </Item>
         </Grid>
     );
 }
 
 const chainInfo = () => {
     return (
-        <Container maxWidth="lg" className="blockchain-component-chainInfo">
-            <Typography variant='subtitle1'>Chain data</Typography>
-            <Grid container spacing={{ xs: 2, md: 4 }}>
+        <Box className="blockchain-component-chainInfo">
+            <Typography variant='h3'>Chain data</Typography>
+            <Grid container spacing={{ xs: 1 }}>
                 {(data.items.map(({ title, value }) => {
                     const [statedTitle] = useState(title);
                     const [statedValue] = useState(value);
                     return ChainInfoCard(statedTitle, statedValue);
                 }))}
             </Grid>
-        </Container>
+        </Box>
     )
 }
 
---
+++ src/blockchain/components/chainInfo.styles.js
@@ -0,0 +1,31 @@
+import { styled } from '@mui/material/styles';
+
+import { 
+    Paper,
+    Typography,
+} from '@mui/material';
+
+export const Item = styled(Paper)(({ theme }) => ({
+    ...theme.typography.body2,
+    padding: theme.spacing(4),
+    color: theme.palette.primary.contrastText,
+    boxShadow: theme.shadows.box,
+    borderRadius: 13,
+    mask: "linear-gradient(#FFF,#FFF)",
+    "&:before": {
+        content: "''",
+        position: "absolute",
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        background: theme.gradient.primary,
+        zIndex: "-1",
+    },
+}));
+
+export const Data = styled(Typography)(({ theme }) => ({
+    fontSize: 33,
+    fontWeight: 900,
+}));
+
