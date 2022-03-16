4d658cef42d20ca1e0178f89b3d365fd42726d29
pixellab <pixellab@gmail.com>
2022-03-16 11:40:12 -0700

    chore: update chicklet styles & add hover state

--- src/blockchain/components/chainInfo.styles.js
+++ src/blockchain/components/chainInfo.styles.js
@@ -9,11 +9,12 @@ export const Item = styled(Paper)(({ theme }) => ({
     ...theme.typography.body2,
     paddingTop: theme.spacing(2.5),
     paddingBottom: theme.spacing(2.5),
-    paddingLeft: theme.spacing(4.5),
-    paddingRight: theme.spacing(4.5),
+    paddingLeft: theme.spacing(5.5),
+    paddingRight: theme.spacing(5.5),
     color: theme.palette.primary.contrastText,
     boxShadow: theme.shadows.box,
     borderRadius: 13,
+    cursor: "pointer",
     mask: "linear-gradient(#FFF,#FFF)",
     "&:before": {
         content: "''",
@@ -25,19 +26,29 @@ export const Item = styled(Paper)(({ theme }) => ({
         background: theme.gradient.primary,
         zIndex: "-1",
     },
+    "&:hover:after": {
+        content: "''",
+        position: "absolute",
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        background: "rgba(0,0,0,0.25)",
+        zIndex: "-1",
+    },
     [theme.breakpoints.down("md")]: {
         paddingTop: theme.spacing(2),
         paddingBottom: theme.spacing(2),
-        paddingLeft: theme.spacing(2.5),
-        paddingRight: theme.spacing(2.5),
+        paddingLeft: theme.spacing(3),
+        paddingRight: theme.spacing(3),
     },
 }));
 
 export const Data = styled(Typography)(({ theme }) => ({
-    fontSize: 28,
-    fontWeight: 900,
+    fontSize: 22,
+    fontWeight: 600,
     [theme.breakpoints.down("md")]: {
-        fontSize: 22,
+        fontSize: 16,
     },
 }));
 
