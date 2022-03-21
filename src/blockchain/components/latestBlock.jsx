1c41ed9536db5877bf1f2f32af850d126734f032
Marcos Pacheco <marcossilvano496@gmail.com>
2022-03-21 16:25:29 -0300

    chore: added latest block subscription call

--- src/blockchain/components/latestBlock.jsx
+++ src/blockchain/components/latestBlock.jsx
@@ -1,53 +1,18 @@
-import { gql, useApolloClient, useSubscription } from "@apollo/client";
+import { gql, useSubscription } from "@apollo/client";
 import AccessTimeIcon from "@mui/icons-material/AccessTime";
 import {
     Box,
     Grid, Link,
     Typography
 } from "@mui/material";
-import React, { useCallback } from "react";
+import React from "react";
 import PaperWithHeader from "./paperWithHeader";
 
-const ON_NEW_BLOCKS = gql`
-subscription NewBlocks {
-    Blocks(order_by: {createdAt: desc}, limit: 1) {
-        hash
-        data
-        createdAt
-        updatedAt
-    }
-}
-`;
 
-const PAGE_LIMIT = 4;
+const PAGE_LIMIT = 4; // TODO replace with 25
 
-const GET_OLDER_BLOCKS = gql`
-query MyQuery($createdAt: timestamptz, $limit: Int) {
-  Blocks(where: {createdAt: {_lt: $createdAt}}, limit: $limit, order_by: {createdAt: desc}) {
-    data
-    hash
-  }
-}
-`;
-
-const statusToIconMap = (status) => {
-    switch (status) {
-    case "pending":
-        return {
-            "label": "pending",
-            "icon": <AccessTimeIcon color="bad" />
-        };
-    default:
-        return {
-            "label": "undefinded",
-            "icon": <AccessTimeIcon color="green" />
-        };
-    }
-};
-
-const ItemHandler = (block) => {
-    const { label, icon } = statusToIconMap(block.status);
-    return <Grid container sx={{ mb: 4 }} key={block.hash}>
+const ItemHandler = ({ block }) => {
+    return <Grid container sx={{ mb: 4 }}>
         <Grid item xs>
             <Link href="" underline="hover" variant="body2">{block.hash}</Link>
             <Box sx={{ mt: 1, }}>
@@ -56,11 +21,12 @@ const ItemHandler = (block) => {
                 </Link>
                 <Typography variant="body3">|</Typography>
                 <Link href="" underline="hover" variant="body3">{block.events} event</Link>
+                {block.newEntry ? "newBlock": "old"}
             </Box>
         </Grid>
         <Grid item xs="auto">
             <Box sx={{ textAlign: "right" }}>
-                <Box aria-label={label}>{icon}</Box>
+                <Box aria-label={block.status || "undefined"}><AccessTimeIcon color="green" /></Box>
                 <Box>
                     <Typography variant="body3">{block.duration}</Typography>
                 </Box>
@@ -69,51 +35,48 @@ const ItemHandler = (block) => {
     </Grid>;
 };
 
-const BlockChain = (props) => {
-    const client = useApolloClient();
-    const [state, setState] = React.useState({
-        blocks: [],
-    });
-
-    const loadOlder = useCallback(async (block) => {
-        const { data } = await client.query({
-            query: GET_OLDER_BLOCKS,
-            variables: { createdAt: block.createdAt, limit: PAGE_LIMIT - 1 }
-        });
-        // TODO handle error and loading
-        if (data) {
-            setState((prevState) => {
-                return { ...prevState, blocks: [block, ...data.Blocks] };
-            });
-        }
-    }, []);
-
+const BlockChain = ({ newBlocks }) => {
+    const [state, setState] = React.useState({ blocks: [] });
     React.useEffect(() => {
-        if (!(state.blocks && state.blocks.length)) {
-            loadOlder(props.newBlocks[0]);
-        } else {
-            setState((prevState) => {
-                const olderBlocks = prevState.blocks.length === PAGE_LIMIT
-                    ? prevState.blocks.slice(props.newBlocks.length)
-                    : [...prevState.blocks];
-                return { ...prevState, blocks: [...props.newBlocks, ...olderBlocks] };
-            });
-        }
-    }, [props.newBlocks]);
+        const oldHashes = state.blocks.map((b) => b.hash);
+        setState((prevState) => {
+            return {
+                ...prevState,
+                blocks: newBlocks.map((block) => {
+                    return {
+                        ...block,
+                        newEntry: oldHashes.length && !oldHashes.includes(block.hash) 
+                    };
+                })
+            };
+        });
+    }, [newBlocks]);
 
-    // TODO receive the new block and 
     return <PaperWithHeader
         header="LATEST BLOCKS"
         linkName={"SEE ALL"}
         linkAddress={"##"}
         height={500}
     >
-        {state.blocks.map(ItemHandler)}
+        {state.blocks.map((b) => {
+            return <ItemHandler block={b} key={b.hash} />;
+        })}
     </PaperWithHeader>;
 };
 
+const NEW_BLOCKS = gql`
+subscription NewBlocks($limit: Int) {
+    # TODO order by block number
+    Blocks(order_by: {createdAt: desc}, limit: $limit) {
+        hash
+        data
+        createdAt
+        updatedAt
+    }
+}`;
+
 const BlockChainRenderer = () => {
-    const { data, error, loading } = useSubscription(ON_NEW_BLOCKS);
+    const { data, error, loading } = useSubscription(NEW_BLOCKS, { variables: { limit: PAGE_LIMIT } });
     if (loading) return <></>;
     if (error) return <></>;
     return <BlockChain newBlocks={data.Blocks} />;
