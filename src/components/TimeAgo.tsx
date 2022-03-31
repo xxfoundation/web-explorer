cce60fc77f0330acf5c2540ba7745998ae1be525
matt-xxnetwork <101591858+matt-xxnetwork@users.noreply.github.com>
2022-04-01 01:50:02 +0700

    feat: add time ago (#13)
    * Add timeago
    
    * Changed units
    
    * Remove 's' for plural
    
    Co-authored-by: BaltasarAroso <baltasar@xx.network>

---
+++ src/components/TimeAgo.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import TimeAgo, { Formatter, ReactTimeagoProps, Unit } from 'react-timeago';
+
+const overrides: Partial<Record<Unit, string>> = {
+  'second': 'sec',
+  'minute': 'min'
+};
+
+const formatterOverride: Formatter = (value, unit, suffix) => {
+  const unitOverride = overrides[unit] ?? unit;
+  return `${value} ${unitOverride} ${suffix}`;
+}
+
+const TimeAgoComponent: React.FC<ReactTimeagoProps> = (props) =>
+  <TimeAgo {...props} formatter={formatterOverride} />
+
+export default TimeAgoComponent;
