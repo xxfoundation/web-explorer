0b6e2ce98ebaf3c443c85d9e9d3ed8b0072302a3
pixellab <pixellab@gmail.com>
2022-04-01 07:48:05 -0700

    chore: header with route conditional

--- src/components/search/Bar.styles.js
+++ src/components/search/Bar.styles.js
@@ -1,14 +1,18 @@
 import { Button, Input, Select } from '@mui/material';
 import { styled } from '@mui/material/styles';
+import { useLocation } from 'react-router-dom';
 
-export const Bar = styled('div')(({ theme }) => ({
-  background: theme.palette.background.transparent,
-  borderRadius: 48,
-  border: theme.borders.light,
-  boxShadow: '0px 15px 34px 3px rgba(0, 0, 0, 0.04)',
-  padding: theme.spacing(2),
-  color: theme.palette.primary.contrastText
-}));
+export const Bar = styled('div')(({ theme }) => { 
+  const { pathname } = useLocation(); 
+  return({
+    background: 'rgba(255,255,255,0.24)',
+    borderRadius: 48,
+    border: pathname == '/' ? theme.borders.light : 'none',
+    boxShadow: '0px 15px 34px 3px rgba(0, 0, 0, 0.04)',
+    padding: theme.spacing(2),
+    color: theme.palette.primary.contrastText
+  });
+});
 
 export const SelectOption = styled(Select)(({ theme }) => ({
   color: theme.palette.primary.contrastText,
