62089978b3248cb2fb20875e13de09f1f7f1635f
pixellab <pixellab@gmail.com>
2022-04-06 12:43:39 -0700

    chore: add table container component

--- src/components/Tables/TableContainer.tsx
+++ src/components/Tables/TableContainer.tsx
@@ -1,22 +1,5 @@
-import { TableContainer } from '@mui/material';
-import { styled } from '@mui/material/styles';
+import { TableStyled } from './TableContainer.styled';
 import { default as React, FC } from 'react';
-
-const TableStyled = styled(TableContainer)(({ theme }) => ({
-  padding: 0, 
-  [theme.breakpoints.down('sm')]: {
-    padding: 0, 
-  },
-  '.MuiTableHead-root': {
-    textTransform: 'uppercase'
-  },
-  '.MuiTableCell-head': {
-    paddingTop: 0
-  },
-  '.MuiTableCell-body': {
-    borderBottom: 'none'
-  }
-}));
 
 export const TableWrap: FC = ({ children }) => (
   <TableStyled>
