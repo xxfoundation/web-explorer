9d6c1be8b1779e2ef0615ec59443b27a240b7d9d
Marcos Pacheco <marcossilvano496@gmail.com>
2022-04-13 16:19:33 -0300

    style: removed background color of search options

--- src/components/search/Bar.styles.ts
+++ src/components/search/Bar.styles.ts
@@ -4,14 +4,14 @@ import { useLocation } from 'react-router-dom';
 
 export const Bar = styled('div')(({ theme }) => {
   const { pathname } = useLocation();
-  return ({
+  return {
     background: 'rgba(255,255,255,0.24)',
     borderRadius: 48,
-    border: pathname == '/' ? theme.borders.light : 'none',
+    border: pathname == '/' ? theme.borders?.light : 'none',
     boxShadow: '0px 15px 34px 3px rgba(0, 0, 0, 0.04)',
     padding: theme.spacing(2),
     color: theme.palette.primary.main
-  });
+  };
 });
 
 export const SelectOption = styled(Select)(({ theme }) => ({
@@ -23,6 +23,9 @@ export const SelectOption = styled(Select)(({ theme }) => ({
   },
   '&:before, &:after': {
     border: 'none !important'
+  },
+  'div, div:focus': {
+    background: 'none'
   },
   svg: {
     color: theme.palette.primary.main
@@ -37,8 +40,9 @@ export const SelectItem = styled(MenuItem)(({ theme }) => ({
   paddingLeft: 20,
   paddingRight: 20,
   color: theme.palette.grey.A700,
-  '&:hover': {
-    color: theme.palette.primary.main,
+  '&:hover, &.Mui-selected, &.Mui-selected:hover': {
+    color: theme.palette.text.secondary,
+    background: 'none'
   }
 }));
 
