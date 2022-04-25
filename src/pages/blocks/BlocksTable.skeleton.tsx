d8b8cf218b20caa4b7632f3b4bd68c26e7d4df35
Marcos Pacheco <marcossilvano496@gmail.com>
2022-04-25 19:43:57 -0300

    refactor: added skeleton on BlocksTable

---
+++ src/pages/blocks/BlocksTable.skeleton.tsx
@@ -0,0 +1,17 @@
+import { Skeleton } from '@mui/material';
+import React, { useMemo } from 'react';
+import { BaseLineCellsWrapper, BaselineTable } from '../../components/Tables';
+import useRows from '../../hooks/useRows';
+
+const BlocksTableSkeleton = () => {
+  const cells = useRows(6).map((Cell) => <Cell />);
+  const row = useMemo(() => cells.map(() => BaseLineCellsWrapper(cells)), [cells]);
+  return (
+    <>
+      <BaselineTable headers={BaseLineCellsWrapper(cells)} rows={row} />
+      <Skeleton />
+    </>
+  );
+};
+
+export default BlocksTableSkeleton;
