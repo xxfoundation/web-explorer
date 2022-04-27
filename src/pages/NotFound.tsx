0c5c8f7baf9cc2a9995544bbfa3283c575afc79e
Marcos Pacheco <marcossilvano496@gmail.com>
2022-04-27 15:06:03 -0300

    chore: responsiveness adjustments

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,66 +1,66 @@
-import { Button, Container, Divider, dividerClasses, Typography } from '@mui/material';
+import {
+  Button,
+  Container,
+  Divider,
+  linkClasses,
+  styled,
+  Typography, useMediaQuery
+} from '@mui/material';
 import React from 'react';
 import SlashStroke from '../assets/images/icons/SlashStroke.svg';
 import Link from '../components/Link';
 import { PaperStyled } from '../components/Paper/PaperWrap.styled';
-import { theme } from '../themes/default';
+
+const TypoTitle = styled(Typography)(({ theme: th }) => {
+  return {
+    fontSize: '49px',
+    fontWeight: 500,
+    background:
+      '-webkit-linear-gradient(68.04deg, #4668BF 14.57%, #2581D6 41.33%, #019CB1 72.19%, #01ACAC 96.47%, #959595 112.54%)',
+    WebkitBackgroundClip: 'text',
+    WebkitTextFillColor: 'transparent',
+    [th.breakpoints.down('sm')]: {
+      fontSize: '30px'
+    }
+  };
+});
+
+const ResponsiveDivider = styled(Divider)(({ theme: th }) => {
+  return {
+    maxWidth: useMediaQuery(th.breakpoints.down('sm')) ? '90%' : '60%',
+    margin: '0 auto'
+  };
+});
+
+const RoundedButton = styled(Button)(({}) => {
+  return {
+    borderRadius: '30px',
+    width: '84px',
+    height: '35px',
+    marginTop: '30px',
+    marginBottom: '50px',
+    [`& .${linkClasses.root}`]: {
+      fontSize: '12px',
+      fontWeight: 500,
+      letterSpacing: 1,
+      color: 'white',
+      '&:hover': {
+        textDecoration: 'none'
+      }
+    }
+  };
+});
 
 const NotFound = () => {
   return (
     <Container sx={{ my: 6, textAlign: 'center' }} maxWidth='sm'>
       <PaperStyled sx={{ paddingY: '60px' }}>
-        <Typography variant='subtitle2'>
-          Hmmm...
-        </Typography>
-        <Typography
-          fontSize={'49px'}
-          fontWeight={500}
-          sx={{
-            background:
-              '-webkit-linear-gradient(68.04deg, #4668BF 14.57%, #2581D6 41.33%, #019CB1 72.19%, #01ACAC 96.47%, #959595 112.54%)',
-            WebkitBackgroundClip: 'text',
-            WebkitTextFillColor: 'transparent'
-          }}
-        >
-          Page Not Found
-        </Typography>
-        <Button
-          sx={{
-            borderRadius: '30px',
-            width: '84px',
-            height: '35px',
-            marginTop: '30px',
-            marginBottom: '50px'
-          }}
-          variant='contained'
-        >
-          <Link
-            to={'/'}
-            sx={{
-              fontSize: '12px',
-              fontWeight: 500,
-              letterSpacing: 1,
-              color: 'white',
-              '&:hover': {
-                textDecoration: 'none'
-              }
-            }}
-          >
-            home
-          </Link>
-        </Button>
-        <Divider
-          flexItem
-          orientation='horizontal'
-          sx={{
-            [`&.${dividerClasses.root}`]: {
-              color: theme.palette.divider,
-              borderColor: theme.palette.divider,
-              maxWidth: '60%',
-              margin: '0 auto'
-            }
-          }}
-        />
+        <Typography variant='subtitle2'>Hmmm...</Typography>
+        <TypoTitle>Page Not Found</TypoTitle>
+        <RoundedButton variant='contained'>
+          <Link to={'/'}>home</Link>
+        </RoundedButton>
+        <ResponsiveDivider flexItem orientation='horizontal' />
         <img src={SlashStroke} alt='not found icon' style={{ margin: '60px auto 20px' }} />
       </PaperStyled>
     </Container>
