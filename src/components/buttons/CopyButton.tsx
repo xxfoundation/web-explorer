c057fd8727e6bb1bca1b8281e474c1354146ed5c
Marcos Pacheco <marcossilvano496@gmail.com>
2022-05-17 15:10:43 -0300

    chore: added copy notification for copy buttons

--- src/components/buttons/CopyButton.tsx
+++ src/components/buttons/CopyButton.tsx
@@ -1,17 +1,35 @@
+import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
 import FileCopyIcon from '@mui/icons-material/FileCopy';
 import FileCopyOutlinedIcon from '@mui/icons-material/FileCopyOutlined';
-import { IconButton } from '@mui/material';
-import React from 'react';
+import { IconButton, Stack, Typography } from '@mui/material';
+import React, { ReactNode } from 'react';
 import useCopyClipboard from '../../hooks/useCopyToClibboard';
+import { theme } from '../../themes/default';
+
+export const callbackCopyMessage = (value: ReactNode) => {
+  return (
+    <Stack spacing={1}>
+      <Stack direction={'row'} alignItems={'center'} spacing={2}>
+        <CheckCircleOutlineIcon sx={{ color: theme.palette.success.light }} />
+        <Typography fontSize={10} fontWeight={600}>
+          Copied to Clipboard
+        </Typography>
+      </Stack>
+      <Typography fontSize={10} fontStyle={'italic'} component={'span'}>
+        {value}
+      </Typography>
+    </Stack>
+  );
+};
 
 const CopyButton: React.FC<{ value: string }> = ({ value }) => {
-  const [isCopied, staticCopy] = useCopyClipboard();
+  const [isCopied, staticCopy] = useCopyClipboard(4000);
   return (
     <IconButton
       size='small'
       arial-label='copy'
       onClick={() => {
-        staticCopy(value);
+        staticCopy(value, callbackCopyMessage);
       }}
     >
       {isCopied ? (
