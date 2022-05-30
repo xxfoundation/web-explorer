0b2f7afd92c9b995b74e889a43032afc9c0e484b
Matt <matt@xx.network>
2022-05-31 00:08:12 +0700

    Rename test variables

--- src/hooks/useNewnessTracker.spec.ts
+++ src/hooks/useNewnessTracker.spec.ts
@@ -4,13 +4,13 @@ import useNewnessTracker from './useNewnessTracker';
 
 type Item = { id: string, value: number };
 const items = [{ id: 'one', value: 1 }, { id: 'two', value: 2 }, { id: 'three', value: 3 }];
-const newItems = [{id: 'one', value: 1 }, { id: 'four', value: 4 }, { id: 'five', value: 5 }];
-const lastItems = [{ id: 'six', value: 6 }, { id: 'seven', value: 7 }, { id: 'four', value: 4 }, { id: 'five', value: 5 }];
+const secondUpdate = [{id: 'one', value: 1 }, { id: 'four', value: 4 }, { id: 'five', value: 5 }];
+const thirdUpdate = [{ id: 'six', value: 6 }, { id: 'seven', value: 7 }, { id: 'four', value: 4 }, { id: 'five', value: 5 }];
 
 test(`${useNewnessTracker.name} hook`, async () => {
   const hook = renderHook((i: Item[] = items) => useNewnessTracker(i, 'id'));
 
-  hook.rerender(newItems);
+  hook.rerender(secondUpdate);
 
   expect(hook.result.current).toEqual([
     { id: 'one', new: false, value: 1 },
@@ -18,7 +18,7 @@ test(`${useNewnessTracker.name} hook`, async () => {
     { id: 'five', new: true, value: 5 }
   ]);
 
-  hook.rerender(lastItems);
+  hook.rerender(thirdUpdate);
 
   expect(hook.result.current).toEqual([
     { id: 'six', new: true, value: 6 },
