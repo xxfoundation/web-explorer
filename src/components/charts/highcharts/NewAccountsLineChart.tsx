c778cc492f2d720a294f23dd4c46bc051f5c1498
Marcos Pacheco <marcossilvano496@gmail.com>
2022-06-01 11:51:37 -0300

    chore: new accounts chart implementation

--- src/components/charts/highcharts/NewAccountsLineChart.tsx
+++ src/components/charts/highcharts/NewAccountsLineChart.tsx
@@ -1,18 +1,32 @@
-import React from 'react';
+import { useSubscription } from '@apollo/client';
+import { Box, CircularProgress } from '@mui/material';
+import React, { useMemo } from 'react';
 import type { DataPoint } from '.';
-import { LineChart, amountByEraTooltip } from '.';
+import { amountByEraTooltip, LineChart } from '.';
+import { LISTEN_FOR_NEW_ACCOUNTS } from '../../../schemas/accounts.schema';
 import ChartWrap from '../ChartWrap';
 
-const data: DataPoint[] = [
-  [123, 1231],
-  [441, 6671],
-  [155, 667]
-];
-
 const NewAccountsChart = () => {
+  const { data, loading } = useSubscription<{ new_accounts: { accounts: number; era: number }[] }>(
+    LISTEN_FOR_NEW_ACCOUNTS
+  );
+
+  const chartData: DataPoint[] = useMemo(
+    () => (data?.new_accounts || []).map((item) => [item.era, item.accounts], [data?.new_accounts]),
+    [data?.new_accounts]
+  );
+  if (loading) {
+    return (
+      <Box
+        sx={{ height: '400px', justifyContent: 'center', alignItems: 'center', display: 'flex' }}
+      >
+        <CircularProgress />
+      </Box>
+    );
+  }
   return (
     <ChartWrap title='New Accounts'>
-      <LineChart tooltipFormatter={amountByEraTooltip} data={data} />
+      <LineChart tooltipFormatter={amountByEraTooltip} data={chartData} />
     </ChartWrap>
   );
 };
