aa84856fb36d6fc2e3e8abdea2c4d40e3087fc39
BaltasarAroso <baltasar@xx.network>
2022-07-22 17:34:48 -0500

    add banner components on mobile

---
+++ src/components/BannerDesktop.tsx
@@ -0,0 +1,42 @@
+import { AppBar, Button, ThemeProvider, Typography } from '@mui/material';
+import React from 'react';
+import { theme } from '../themes/default';
+import Link from './Link';
+
+export default function BannerDesktop() {
+  return (
+    <AppBar
+      sx={{
+        display: { sm: 'flex', xs: 'none' },
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        textAlign: 'center',
+        backgroundColor: 'ActiveCaption'
+      }}
+    >
+      <Typography variant='body2' sx={{ p: '1em', pr: '2em' }}>
+        If you want to interact with the <b>xx network blockchain</b> use our web based wallet app
+        (formally known as the explorer)
+      </Typography>
+      <ThemeProvider theme={theme}>
+        <Button
+          component={Link}
+          to='https://explorer.xx.network'
+          variant='contained'
+          color='primary'
+          fontSize='12px'
+          padding='1em'
+          minWidth='fit-content !important'
+          margin='auto'
+          marginRight='1em !important'
+          height='2em'
+          target='_blank'
+          rel='noopener'
+        >
+          xx wallet
+        </Button>
+      </ThemeProvider>
+    </AppBar>
+  );
+}
--- src/index.tsx
+++ src/index.tsx
@@ -8,44 +8,14 @@ import './index.css';
 import { client as apolloClient } from './plugins/apollo';
 import reportWebVitals from './reportWebVitals';
 import SnackbarProvider from './SnackbarProvider';
-import { AppBar, Typography, Button, ThemeProvider } from '@mui/material';
-import Link from './components/Link';
-import { theme } from './themes/default';
+import BannerDesktop from './components/BannerDesktop';
+import BannerMobile from './components/BannerMobile';
 
 ReactDOM.render(
   <React.StrictMode>
     <SnackbarProvider>
-      <AppBar
-        sx={{
-          display: 'flex',
-          flexDirection: 'row',
-          justifyContent: 'center',
-          alignItems: 'center',
-          textAlign: 'center',
-          backgroundColor: 'ActiveCaption'
-        }}
-      >
-        <Typography variant='body2' sx={{ p: '1em', pr: '2em' }}>
-          If you want to interact with the <b>xx network blockchain</b> use our web based wallet app
-          (formally known as the explorer)
-        </Typography>
-        <ThemeProvider theme={theme}>
-          <Button
-            component={Link}
-            to='https://explorer.xx.network'
-            variant='contained'
-            color='primary'
-            fontSize='12px'
-            padding='1em'
-            margin='auto'
-            height='2em'
-            target='_blank'
-            rel='noopener'
-          >
-            xx wallet
-          </Button>
-        </ThemeProvider>
-      </AppBar>
+      <BannerDesktop />
+      <BannerMobile />
       <ApolloProvider client={apolloClient}>
         <App />
       </ApolloProvider>
