b9af8715c90c6b0faae05bdfa1a967a6e19d116c
BaltasarAroso <baltasar@xx.network>
2022-08-05 12:32:21 -0500

    add ActionSelection page

--- src/pages/staking/simple/SimpleStaker.tsx
+++ src/pages/staking/simple/SimpleStaker.tsx
@@ -1,16 +1,17 @@
-import React, { FC, useCallback, useMemo, useState } from 'react';
+import React, { FC, useCallback, useEffect, useMemo, useState } from 'react';
 import PaperWrap from '../../../components/Paper/PaperWrap.styled';
-import { Box, Tabs, Tab,TabProps, Stack } from '@mui/material';
+import { Box, Tabs, Tab, TabProps, Stack } from '@mui/material';
 
 import ConnectWallet from './ConnectWallet';
 import WalletSelection from './WalletSelection';
 import useAccounts from '../../../hooks/useAccounts';
 import NavButtons, { NavProps } from './NavButtons';
+import ActionSelection from './ActionSelection';
 
 type PanelProps = NavProps & {
   currentStep: number;
   step: number;
-}
+};
 
 const Panel: FC<PanelProps & NavProps> = ({ children, currentStep, step, ...navProps }) => {
   return (
@@ -29,9 +30,9 @@ const Panel: FC<PanelProps & NavProps> = ({ children, currentStep, step, ...navP
       )}
     </div>
   );
-}
+};
 
-const tabProps = (index: number): TabProps => { 
+const tabProps = (index: number): TabProps => {
   return {
     id: `vertical-tab-${index}`,
     'aria-controls': `vertical-tabpanel-${index}`,
@@ -44,22 +45,27 @@ const tabProps = (index: number): TabProps => {
         borderWidth: '1px',
         borderStyle: 'solid'
       }
-    },
+    }
   };
-}
+};
 
 const MAX_STEPS = 5;
+export type StakingOptions = 'stake' | 'unstake' | 'redeem';
 
 const VerticalTabs = () => {
   const accounts = useAccounts();
   const [selectedAccount, setSelectedAccount] = useState('');
+  const [selectedStakingOption, setSelectedStakingOption] = useState<StakingOptions>();
   const [step, setStep] = useState(0);
 
-  const validSteps = useMemo<Record<number, boolean>>(() => ({
+  const validSteps = useMemo<Record<number, boolean>>(
+    () => ({
       0: true,
       1: accounts.hasAccounts,
       2: !!selectedAccount
-  }), [accounts.hasAccounts, selectedAccount]);
+    }),
+    [accounts.hasAccounts, selectedAccount]
+  );
 
   const next = useCallback(() => {
     setStep((v) => Math.min(MAX_STEPS - 1, v + 1));
@@ -69,28 +75,36 @@ const VerticalTabs = () => {
     setStep((v) => Math.max(0, v - 1));
   }, []);
 
-  const onChange = useCallback((evt: React.SyntheticEvent, value: any) => {
-    const i = Number(value);
-    if (validSteps[i]) {
-      setStep(i);
-    }
-  }, [validSteps])
+  const onChange = useCallback(
+    (evt: React.SyntheticEvent, value: any) => {
+      const i = Number(value);
+      if (validSteps[i]) {
+        setStep(i);
+      }
+    },
+    [validSteps]
+  );
 
-  const panelProps = useCallback((index: number): PanelProps => ({
-    onNext: index < MAX_STEPS ? next : undefined,
-    canNext: !!validSteps[index + 1],
-    onBack: index > 0 ? back : undefined,
-    canBack: !!validSteps[index - 1],
-    step: index,
-    currentStep: step,
-  }), [back, next, step, validSteps]);
-  
+  const panelProps = useCallback(
+    (index: number): PanelProps => ({
+      onNext: index < MAX_STEPS ? next : undefined,
+      canNext: !!validSteps[index + 1],
+      onBack: index > 0 ? back : undefined,
+      canBack: !!validSteps[index - 1],
+      step: index,
+      currentStep: step
+    }),
+    [back, next, step, validSteps]
+  );
+
+  useEffect(() => {
+    if (selectedStakingOption) {
+      next();
+    }
+  }, [next, selectedStakingOption]);
 
   return (
-    <Stack
-      direction='row'
-      sx={{ bgcolor: 'background.paper' }}
-    >
+    <Stack direction='row' sx={{ bgcolor: 'background.paper' }}>
       <Tabs
         orientation='vertical'
         value={step}
@@ -110,34 +124,24 @@ const VerticalTabs = () => {
         <Tab label='Step 05' {...tabProps(4)} />
       </Tabs>
       <Panel {...panelProps(0)}>
-        <ConnectWallet />
+        <ActionSelection onSelect={setSelectedStakingOption} />
       </Panel>
       <Panel {...panelProps(1)}>
-        <WalletSelection
-          onSelect={setSelectedAccount}
-          selected={selectedAccount}  />
+        <WalletSelection onSelect={setSelectedAccount} selected={selectedAccount} />
       </Panel>
-      <Panel {...panelProps(2)}>
-        Item Three
-      </Panel>
-      <Panel {...panelProps(3)}>
-        Item Four
-      </Panel>
-      <Panel {...panelProps(4)}>
-        Item Five
-      </Panel>
+      <Panel {...panelProps(2)}>Item Three</Panel>
+      <Panel {...panelProps(3)}>Item Four</Panel>
+      <Panel {...panelProps(4)}>Item Five</Panel>
     </Stack>
   );
-}
-
-
+};
 
 const SimpleStaker = () => {
   return (
     <PaperWrap sx={{ p: { xs: 0, sm: 0, md: 0 }, overflow: 'hidden' }}>
       <VerticalTabs />
     </PaperWrap>
-  )
-}
+  );
+};
 
 export default SimpleStaker;
