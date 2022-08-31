5d19c57b90d8d23e91425efae666aa2ef2e3b029
matt-xxnetwork <101591858+matt-xxnetwork@users.noreply.github.com>
2022-08-31 13:12:00 -0300

    Align code

--- src/pages/staking/StakingCharts.tsx
+++ src/pages/staking/StakingCharts.tsx
@@ -9,40 +9,40 @@ import PaperWrapStyled from '../../components/Paper/PaperWrap.styled';
 
 const StakingCharts = () => {
   return (
-<Grid container spacing={2}>
-  <Grid item xs={12} md={6}>
-    <PaperWrapStyled>
-      <Typography variant='h3' gutterBottom>
-        Average Points
-      </Typography>
-      <AveragePointsLineChart />
-    </PaperWrapStyled>
-  </Grid>
-  <Grid item xs={12} md={6}>
-    <PaperWrapStyled>
-      <Typography variant='h3' gutterBottom>
-        Average Performance
-      </Typography>
-      <AveragePerformanceLineChart />
-    </PaperWrapStyled>
-  </Grid>
-  <Grid item xs={12} md={6}>
-    <PaperWrapStyled>
-      <Typography variant='h3' gutterBottom>
-        Average Self Stake
-      </Typography>
-      <AverageSelfStakeLineChart />
-    </PaperWrapStyled>
-  </Grid>
-  <Grid item xs={12} md={6}>
-    <PaperWrapStyled>
-      <Typography variant='h3' gutterBottom>
-        Average Commission
-      </Typography>
-      <AverageCommissionLineChart />
-    </PaperWrapStyled>
-  </Grid>
-</Grid>
+    <Grid container spacing={2}>
+      <Grid item xs={12} md={6}>
+        <PaperWrapStyled>
+          <Typography variant='h3' gutterBottom>
+            Average Points
+          </Typography>
+          <AveragePointsLineChart />
+        </PaperWrapStyled>
+      </Grid>
+      <Grid item xs={12} md={6}>
+        <PaperWrapStyled>
+          <Typography variant='h3' gutterBottom>
+            Average Performance
+          </Typography>
+          <AveragePerformanceLineChart />
+        </PaperWrapStyled>
+      </Grid>
+      <Grid item xs={12} md={6}>
+        <PaperWrapStyled>
+          <Typography variant='h3' gutterBottom>
+            Average Self Stake
+          </Typography>
+          <AverageSelfStakeLineChart />
+        </PaperWrapStyled>
+      </Grid>
+      <Grid item xs={12} md={6}>
+        <PaperWrapStyled>
+          <Typography variant='h3' gutterBottom>
+            Average Commission
+          </Typography>
+          <AverageCommissionLineChart />
+        </PaperWrapStyled>
+      </Grid>
+    </Grid>
   );
 };
 
