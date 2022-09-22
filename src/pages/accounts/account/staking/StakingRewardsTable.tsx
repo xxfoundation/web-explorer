a0e180cd793d3280bb200d7044bb20de2ed77941
Bernardo Cardoso <bernardo@xx.network>
2022-09-22 18:07:57 +0000

    Implement CSV download button for staking rewards

--- src/pages/accounts/account/staking/StakingRewardsTable.tsx
+++ src/pages/accounts/account/staking/StakingRewardsTable.tsx
@@ -1,6 +1,7 @@
 import { useQuery } from '@apollo/client';
-import { Typography } from '@mui/material';
-import React, { FC, useEffect, useMemo } from 'react';
+import { Divider, Stack, Typography } from '@mui/material';
+import React, { FC, useEffect, useMemo, useState } from 'react';
+import dayjs from 'dayjs';
 import FormatBalance from '../../../../components/FormatBalance';
 import XXNetworkAddress from '../../../../components/Hash/XXNetworkAddress';
 import Link from '../../../../components/Link';
@@ -12,6 +13,8 @@ import {
   GET_STAKING_REWARDS,
   StakingReward
 } from '../../../../schemas/staking.schema';
+import DownloadDataButton from '../../../../components/buttons/DownloadDataButton';
+import { ExportToCsv } from 'export-to-csv';
 
 const DEFAULT_ROWS_PER_PAGE = 10;
 const headers = BaseLineCellsWrapper(['Validator', 'Block Number', 'Era', 'Amount', 'Timestamp']);
@@ -30,6 +33,19 @@ const RewardsRow = (reward: StakingReward) => {
   ]);
 };
 
+interface CSVRow {
+  'Payout Date': string;
+  'Block Number': number;
+  'Reward Date': string;
+  'Era': number;
+  'Validator Address': string;
+  'Validator ID': string;
+  'Amount': number;
+}
+
+const eraTime = 86400000;
+const genesisTime = 1637132496000;
+
 const StakingRewardsTable: FC<{
   accountId: string;
   sum?: number;
@@ -41,11 +57,36 @@ const StakingRewardsTable: FC<{
   const pagination = usePagination({ rowsPerPage: DEFAULT_ROWS_PER_PAGE });
   const { paginate, setCount } = pagination;
 
+  const options = {
+    filename: 'rewards_' + accountId,
+    fieldSeparator: ',',
+    quoteStrings: '"',
+    decimalSeparator: '.',
+    showLabels: true,
+    showTitle: false,
+    useTextFile: false,
+    useBom: true,
+    useKeysAsHeaders: true,
+  };
+  const [csvData, setCsvData] = useState<CSVRow[]>();
+  const csvExporter = new ExportToCsv(options);
+
   useEffect(() => {
     if (stakingRewards.data?.aggregates?.aggregate) {
       setCount(stakingRewards.data?.aggregates?.aggregate.count);
+      setCsvData(stakingRewards.data?.rewards.map((el) => {
+        return {
+          'Payout Date': dayjs.utc(el.timestamp).format('ll LTS Z'),
+          'Block Number': el.blockNumber,
+          'Reward Date': dayjs.utc((el.era+1)*eraTime+genesisTime).format('ll LTS Z'),
+          'Era': el.era,
+          'Validator Address': el.validatorAddress,
+          'Validator ID': el.identity?.display || '',
+          'Amount': el.amount / 1e9,
+        }
+      }))
     }
-  }, [setCount, stakingRewards.data?.aggregates?.aggregate]);
+  }, [setCount, setCsvData, stakingRewards.data]);
 
   const paginated = useMemo(
     () => rewards && paginate(rewards).map(RewardsRow),
@@ -67,7 +108,19 @@ const StakingRewardsTable: FC<{
         headers={headers}
         rows={paginated ?? []}
         rowsPerPage={pagination.rowsPerPage}
-        footer={pagination.controls}
+        footer={(
+          <>
+            <Divider />
+            <Stack direction={'row'} sx={{ justifyContent: 'space-between' }}>
+              <DownloadDataButton onClick={() => {
+                csvExporter.generateCsv(csvData);
+              }}>
+                CSV
+              </DownloadDataButton>
+              {pagination.controls}
+            </Stack>
+          </>
+        )}
       />
     </>
   );
