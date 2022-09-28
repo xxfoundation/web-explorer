b7db963fe06d58b9e52dd163c99e0425b106784c
Baltasar Aroso <baltasar@xx.network>
2022-09-28 19:58:03 +0100

    fix validator list identity display

--- src/pages/staking/simple/utils/AccountDisplay.tsx
+++ src/pages/staking/simple/utils/AccountDisplay.tsx
@@ -1,4 +1,4 @@
-import { GetDisplayIdentity, GET_DISPLAY_IDENTITY } from '../../../../schemas/accounts.schema';
+import { AccountRoles, GetDisplayIdentity, GET_DISPLAY_IDENTITY } from '../../../../schemas/accounts.schema';
 import React, { FC } from 'react';
 import Address from '../../../../components/Hash/XXNetworkAddress';
 import { useQuery } from '@apollo/client';
@@ -13,9 +13,10 @@ type Props = {
 
 const AccountDisplay: FC<Props> = ({ account, balance, targetBlank = false }) => {
   const {data, loading} = useQuery<GetDisplayIdentity>(GET_DISPLAY_IDENTITY, { variables: { account }});
-  const display = data?.identity && data?.identity.length && data?.identity[0].display || undefined;
-  const roles = data?.identity && data?.identity.length && data?.identity[0].account.roles || {};
-
+  const display = data?.roles && data?.roles.length && data?.roles[0].account?.identity?.display || undefined;
+  const rolesAux = data?.roles && data?.roles[0];
+  // TODO: improve this code
+  const roles: Partial<AccountRoles> = Object.assign({}, {council: rolesAux?.council, nominator: rolesAux?.nominator, special: rolesAux?.special, techcommit: rolesAux?.techcommit, validator: rolesAux?.validator});
   const loadingQuery = loading;
 
   return loadingQuery ? (
