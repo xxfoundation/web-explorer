5a73b84c6ff140bca5bf4b79917520cc217cb18b
Bernardo Cardoso <bernardo@xx.network>
2024-06-10 15:56:01 +0000

    Update discord link to discord.xx.network

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -163,7 +163,7 @@ const Footer = () => {
                   twitter: 'xx_network',
                   github: 'xx-labs',
                   telegram: 'xxnetwork',
-                  discord: 'qKWaRCKNqr'
+                  discord: 'https://discord.xx.network'
                 }}
               />
             </Grid>
--- src/pages/accounts/Summary.tsx
+++ src/pages/accounts/Summary.tsx
@@ -102,7 +102,7 @@ const ContactInfo = () => {
             twitter: 'xx_network',
             github: 'xx-labs',
             telegram: 'xxnetwork',
-            discord: 'qKWaRCKNqr'
+            discord: 'https://discord.xx.network'
           }}
         />
       </Grid>
